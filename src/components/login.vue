c45f1aa218a0a2453fda0eea9bd3c7a393cd268e
Jerome-chane <cmh.jerome@gmail.com>
2020-01-31 15:47:55 +0100

    update

--- src/components/login.vue
+++ src/components/login.vue
@@ -134,7 +134,7 @@ export default {
     login(event) {
       if (this.email.length >= 3 && this.pwd.length >= 2) {
         this.$store.dispatch("login");
-        setTimeout(this.sync, 500);
+        setTimeout(this.sync, 1000);
         setTimeout(this.connect, 1000);
         event.preventDefault();
       }
